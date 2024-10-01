8cb067f6242f3fa06f6095a36a48e2f59a18eff7
renardbergson <renardrock@gmail.com>
2024-10-01 11:36:21 -0300

    adicionando requisicao ao botao 'sugerir ortografia'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const serverURI = "http://localhost:8080";
+
 document.getElementById("uploadForm").addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -18,18 +20,40 @@ document.getElementById("uploadForm").addEventListener("submit", (e) => {
     body: formData
   }
 
-  fetch("http://localhost:8080/file-upload", configs)
+  fetch(`${serverURI}/file-upload`, configs)
   .then(response => response.json())
   .then(data => {
-    const textArea = document.querySelector("#text-area");
-    textArea.innerHTML = data.text;
-
-    const sections = document.querySelectorAll("section");
-
-    sections.forEach(section => {
-      section.classList.remove("visible");
-      sections[1].classList.add("visible");
-    })
+    handleTextArea(data.text)
+    handleSections(1);
   })
   .catch(error => console.error("Erro:", error));
 })
+
+document.getElementById("reviseBtn").addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const response = await fetch(`${serverURI}/text-correction`, {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify({text: textArea.value})
+  });
+
+  const result = await response.json();
+  handleTextArea(result);
+})
+
+function handleSections(index) {
+  const sections = document.querySelectorAll("section");
+
+  sections.forEach(section => {
+    section.classList.remove("visible");
+    sections[index].classList.add("visible");
+  })
+}
+
+function handleTextArea(text) {
+  const textArea = document.querySelector("#text-area");
+  textArea.innerHTML = text;
+}
